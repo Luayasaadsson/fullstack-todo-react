32560e9508ea347475b16b25d7361a1cd4810f20
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-06-16 01:05:48 +0200

    CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,8 +7,19 @@ import todoRoutes from "./routes/todos";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://fullstack-todolist-react.netlify.app",
+  ],
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(cors());
 
 // Routes
 app.use("/users", userRoutes);
