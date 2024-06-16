cc940d73471025faeea308b352ac7abc2b33f7f0
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-06-16 22:59:01 +0200

    .

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,17 +10,18 @@ const users_1 = __importDefault(require("./routes/users"));
 const todos_1 = __importDefault(require("./routes/todos"));
 const app = (0, express_1.default)();
 const PORT = Number(process.env.PORT) || 3000;
-const corsOptions = {
-    origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://fullstack-todolist-react.netlify.app",
-    ],
-    optionsSuccessStatus: 200,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-};
-app.use((0, cors_1.default)(corsOptions));
+/* const corsOptions = {
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://fullstack-todolist-react.netlify.app",
+  ],
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}; */
+/* app.use(cors(corsOptions)); */
+app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
 // Routes
 app.use("/users", users_1.default);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import todoRoutes from "./routes/todos";
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-const corsOptions = {
+/* const corsOptions = {
   origin: [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
@@ -16,9 +16,10 @@ const corsOptions = {
   optionsSuccessStatus: 200,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
-};
+}; */
 
-app.use(cors(corsOptions));
+/* app.use(cors(corsOptions)); */
+app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
