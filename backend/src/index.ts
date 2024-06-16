1298b89cabe1a0d333cf4dc5874208e89407ce23
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-06-16 22:15:24 +0200

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,9 +25,9 @@ app.use(bodyParser.json());
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
