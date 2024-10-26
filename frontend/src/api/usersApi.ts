ceff64782b033fe2c436a4241d08ed809818d764
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-10-26 21:55:24 +0200

    Updates

--- frontend/src/api/usersApi.ts
+++ frontend/src/api/usersApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/users",
+  baseURL: import.meta.env.VITE_API_URL + "/users",
 });
 
 // Registrerar en ny användare
